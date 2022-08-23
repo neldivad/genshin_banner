89d0aa9e84ca188c3976e4993ed6b2ab9213386e
David L.E <59020919+neldivad@users.noreply.github.com>
2022-08-24 03:18:17 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,6 +3,7 @@ import streamlit.components.v1 as components
 from streamlit_player import st_player
 from streamlit_option_menu import option_menu
 
+import numpy as np
 import pandas as pd
 import plotly
 
@@ -14,7 +15,7 @@ from gsheetsdb import connect
 #-------------------------------------------
 # Run app 
 #-------------------------------------------
-current_date = datetime.now(pytz.timezone('US/Eastern'))
+st.session_state.current_date = datetime.today().strftime('%Y-%m-%d')
 
 def main():
   hide_st_style = """
@@ -24,7 +25,8 @@ def main():
               header {visibility: hidden;}
               </style>
               """
-  st.markdown(hide_st_style, unsafe_allow_html=True)    
+
+  # st.markdown(hide_st_style, unsafe_allow_html=True)    
 
   # st.write('^ this is a placeholder image')
 
@@ -53,32 +55,86 @@ def main():
       pass
 
   #----------------
-  
-  # st.write(current_date)
-
   sheet_url = 'https://docs.google.com/spreadsheets/d/1EGl6QfXxbinI4z8WqJ80Gu5lyQhfUy9KuLVPJNaruIA/edit?pli=1#gid=0'
   df = df_from_gsheet(sheet_url)
+  
+  current_date = pd.to_datetime( st.session_state.current_date )
+  df['start_date'] = pd.to_datetime( df['start_date'])
+  df['end_date'] = pd.to_datetime( df['end_date'])
+  df['days_since_last_banner'] = ( (current_date - df['end_date']) / np.timedelta64(1, 'D')).astype(int)
 
-  # st.write(df)
+  anemos = ['Venti', 'Xiao', 'Kazuha']
+  cryos = ['Ganyu', 'Eula', 'Ayaka', 'Shenhe']
+  dendros = ['Tighnari']
+  electros = ['Keqing', 'Raiden', 'Yae']
+  geos = ['Zhongli', 'Albedo', 'Itto']
+  hydros = ['Tartaglia', 'Kokomi', 'Ayato','Yelan']
+  pyros = ['Klee', 'Hutao', 'Yoimiya']
 
+  element_dict = {
+    'Anemo': anemos, 
+    'Cryo': cryos, 
+    'Dendro': dendros,
+    'Electro': electros,
+    'Geo': geos,
+    'Hydro': hydros,
+    'Pyro': pyros,
+  }
+
+  color_discrete_map = {
+    'Anemo': '#40F0AF', 
+    'Cryo': '#A9DEF9', 
+    'Dendro': '#277559',
+    'Electro': '#67489A',
+    'Geo': '#CBA328',
+    'Hydro': '#4558EB',
+    'Pyro': '#E1081E', 
+  }
+
+  color_discrete_map_char = {
+    '#40F0AF': anemos, 
+    '#A9DEF9': cryos, 
+    '#277559':  dendros,
+    '#67489A': electros,
+    '#CBA328': geos,
+    '#4558EB': hydros,
+    '#E1081E': pyros, 
+  }
+
+  def flatten_dict(d):
+    nd = {}
+    for k,v in d.items():
+      # Check if it's a list, if so then iterate through
+      if ((hasattr(v, '__iter__') and not isinstance(v, str))):
+        for item in v:
+          nd[item] = k
+      else:
+        nd[v] = k
+    return nd
+
+  flat_d = flatten_dict(element_dict)
+  flat_color = flatten_dict(color_discrete_map_char)
+  df['element'] = df['character'].map(flat_d)
+
+  st.write(df)
   ordering = list( df['character'].unique() )
-  # st.write(len(ordering))
 
-  fig2 = make_timeline_chart(
+  fig = make_timeline_chart(
     df,
     xstart= 'start_date',
     xend= 'end_date',
     ydata= 'character',
     cdata= 'character',
+    hdata= ['days_since_last_banner'],
+    color_discrete_map=flat_color,
     ordering=ordering,
   )
-  st.plotly_chart(fig2, use_container_width=True)
-
+  st.plotly_chart(fig, use_container_width=True)
 
 #----
 # Create a connection object.
 #------
-@st.cache(ttl=600)
+# @st.cache(ttl=600, allow_output_mutation=True)
 def df_from_gsheet(url):
   import gsheetsdb
   conn = connect() 
@@ -86,11 +142,11 @@ def df_from_gsheet(url):
   df_gsheet = pd.DataFrame(rows)
   return df_gsheet
 
-def make_timeline_chart(df, xstart, xend, ydata, cdata, ordering, legend=True):
+def make_timeline_chart(df, xstart, xend, ydata, cdata, hdata, ordering, color_discrete_map='', legend=True):
   import plotly.express as px
 
   n = len(ordering)
-  height = 100 + 20*n
+  height = 200 + 20*n
 
   fig = px.timeline(
     df,
@@ -98,10 +154,13 @@ def make_timeline_chart(df, xstart, xend, ydata, cdata, ordering, legend=True):
     x_end=xend,
     y=ydata,
     color=cdata,
+    hover_data=hdata,
+    color_discrete_map=color_discrete_map,
     category_orders= { 
       ydata: ordering
     },
   )
+
   fig.update_layout(
     height=height,
     title= f'Displaying selected <b>{ydata}</b>,</b>',
@@ -114,7 +173,7 @@ def make_timeline_chart(df, xstart, xend, ydata, cdata, ordering, legend=True):
       titlefont_size=10,
       tickfont_size=10,
     ),
-    hovermode="x unified",
+    #hovermode="x unified",
     showlegend= legend,
     legend=dict(
       title_text=f'{cdata.upper()}',
@@ -134,18 +193,22 @@ def make_timeline_chart(df, xstart, xend, ydata, cdata, ordering, legend=True):
     title_text= '',
     rangeselector=dict(
       buttons=list([
+        dict(count=7, label="1w", step="day", stepmode="backward"),
         dict(count=1, label="1m", step="month", stepmode="backward"),
         dict(count=6, label="6m", step="month", stepmode="backward"),
         dict(count=1, label="YTD", step="year", stepmode="todate"),
         dict(count=1, label="1y", step="year", stepmode="backward"),
         dict(count=5, label="5y", step="year", stepmode="backward"),
-        dict(count=10, label="10y", step="year", stepmode="backward"),
         dict(step="all")
         ])
     ),
     rangeslider= dict(visible=True,),
   )
-  fig.add_vline(x= current_date,line_dash= 'dot')
+  fig.add_vline(x= st.session_state.current_date,line_dash= 'dot')
+  # fig.update_traces(
+  #   marker=dict(
+  #     color=['red', 'blue', 'green', 'yellow'])
+  # )
   return fig
 
 #-----------
