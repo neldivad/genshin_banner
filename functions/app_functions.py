a29cf38d19b5bcc80452f6ff57e2e308f4400bae
David L.E <59020919+neldivad@users.noreply.github.com>
2022-08-23 21:25:33 +0800

    Update app_functions.py

--- functions/app_functions.py
+++ functions/app_functions.py
@@ -3,6 +3,7 @@
 #-----------------------
 def df_from_gsheet(spreadsheet_id, sheetname):
   import pygsheets
+  from google.oauth2 import service_account
   
   credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
